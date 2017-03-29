f72ee2e5df8d0866f5376f47adc51f6531e16322
Adamas Zhu <adamas.zhu@outware.com.au>
2017-03-29 17:10:10 +1100

    Create the message support module.

---
+++ OMStarWars/Modules/Message/Message.swift
@@ -0,0 +1,25 @@
+//  Copyright © 2017 Outware Mobile. All rights reserved.
+
+/**
+ * Add additional support for the message.
+ */
+extension UIViewController {
+    
+    static let LocalizationMessage = "Messages"
+    static let Error = "Error"
+    static let Ok = "Ok"
+    
+    /**
+     * Show an error message.
+     */
+    func showError(error: Error) {
+        let title = NSLocalizedString(UIViewController.Error, tableName: UIViewController.LocalizationMessage, comment: "")
+        let alert = UIAlertController(title: title, message: error.localizedDescription, preferredStyle: .alert)
+        let actionTitle = NSLocalizedString(UIViewController.Ok, tableName: UIViewController.LocalizationMessage, comment: "")
+        let action = UIAlertAction(title: actionTitle, style: .default) { _ in }
+        alert.addAction(action)
+        self.present(alert, animated: true, completion: nil)
+    }
+}
+
+import UIKit
