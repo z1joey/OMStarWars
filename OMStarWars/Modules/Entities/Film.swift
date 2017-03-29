1de421d88a9d00be28891a76d61dec4920be26c0
Adamas Zhu <adamas.zhu@outware.com.au>
2017-03-29 16:08:52 +1100

    Add the Film entity.

---
+++ OMStarWars/Modules/Entities/Film.swift
@@ -0,0 +1,25 @@
+//  Copyright © 2017 Outware Mobile. All rights reserved.
+
+/**
+ * The information about a film.
+ */
+class Film {
+    var title: String
+    var episodeId: Int
+    var openingCrawl: String
+    var director: String
+    var producer: String
+    var releaseDate: Date
+    
+    init() {
+        title = ""
+        episodeId = 0
+        openingCrawl = ""
+        director = ""
+        producer = ""
+        releaseDate = Date()
+    }
+}
+
+import Foundation
+
