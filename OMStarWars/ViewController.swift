734e4d9574dbe04aeef2c2b48af5c8d38dc15a1a
Adamas Zhu <adamas.zhu@outware.com.au>
2017-03-29 17:14:42 +1100

    Working on the list.

--- OMStarWars/ViewController.swift
+++ OMStarWars/ViewController.swift
@@ -6,20 +6,23 @@
 //  Copyright © 2017 Outware Mobile. All rights reserved.
 //
 
-import UIKit
-
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func createGetFilmProducer() -> SignalProducer<Array<Film>, AnyError> {
+        let signal = Signal<Array<Film>, AnyError>.pipe()
+        let producer = SignalProducer<Array<Film>, AnyError>()
     }
 
 
 }
+
+import ReactiveCocoa
+import ReactiveSwift
+import Result
+import UIKit
 
