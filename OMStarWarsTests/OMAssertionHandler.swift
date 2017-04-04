3e6e644e96b1b24e61d436e12e197b1e5e433278
Adamas Zhu <adamas.zhu@outware.com.au>
2017-04-05 09:26:45 +1000

    Configuring the test.

--- OMStarWarsTests/OMAssertionHandler.swift
+++ OMStarWarsTests/OMAssertionHandler.swift
@@ -1,12 +1,12 @@
-//  Copyright © 2017 Outware Mobile. All rights reserved.
-
-class OMAssertionHandler : AssertionHandler {
-    func assert(_ assertion: Bool, message: FailureMessage, location: SourceLocation) {
-        if (!assertion) {
-            print("Expectation failed: \(message.stringValue)")
-        }
-    }
-}
-
-import Foundation
-import Nimble
+////  Copyright © 2017 Outware Mobile. All rights reserved.
+//
+//class OMAssertionHandler : AssertionHandler {
+//    func assert(_ assertion: Bool, message: FailureMessage, location: SourceLocation) {
+//        if (!assertion) {
+//            print("Expectation failed: \(message.stringValue)")
+//        }
+//    }
+//}
+//
+//import Foundation
+//import Nimble
